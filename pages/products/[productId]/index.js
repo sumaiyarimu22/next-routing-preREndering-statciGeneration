a8b2558ecd6279c10c3d5c89dffa2a46acac6f7b
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-03-24 13:44:07 +0600

    Programmatically navigate added

--- pages/products/[productId]/index.js
+++ pages/products/[productId]/index.js
@@ -1,12 +1,18 @@
 import { useRouter } from "next/router";
 const ProductDetails = () => {
-  const {
-    query: { productId },
-  } = useRouter();
+  const router = useRouter();
+
+  const { productId } = router.query;
+
+  const handleOrder = () => {
+    console.log("Order placed successfully");
+    router.push("/products");
+  };
 
   return (
     <div>
       <h1>ProductDetails: {productId}</h1>
+      <button onClick={handleOrder}>place order</button>
     </div>
   );
 };
